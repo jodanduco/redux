ea9059e609aa3271576b1ff4768fd222f07eab81
Josue Duran <josue.duran@globant.com>
2018-03-30 13:30:52 -0600

    Making a reausable chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,20 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+      </tr>
+    );
+  }
   render() {
     return (
       <table className="table table-hover">
@@ -13,12 +26,13 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
 }
 
-function mapStateToProps({ weather }) {
+function mapStateToProps({ weather }) { //-> state
   return { weather }; // ->  return { weather: state.weather };
 }
 
