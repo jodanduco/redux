13d77df7aeffd3398a4fd8c7f15f6712878ddbe0
Josue Duran <josue.duran@globant.com>
2018-04-09 22:32:22 -0500

    Action creator shortcuts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+  /**
+   * This method is called after DOM is ready
+   * with strict spelling
+   */
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return (
       <div>
@@ -9,5 +18,5 @@ class PostIndex extends Component {
     );
   }
 }
-
-export default PostIndex;
+// Action creator shortcut -> props 
+export default connect(null, { fetchPosts })(PostIndex);
