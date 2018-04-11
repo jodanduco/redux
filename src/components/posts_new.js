afa5449dc0701f2cdbf3583bbe15986557430d3c
Josue Duran <josue.duran@globant.com>
2018-04-11 18:59:35 -0500

    Validates inputs values

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -25,25 +25,44 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
-          component={this.renderField}
+          component={this.renderField} //--> Pass function reference
         />
         <Field
-          label="Tags"
-          name="tags"
-          component={this.renderField} //--> Pass function reference
+          label="Categories"
+          name="categories"
+          component={this.renderField}
         />
         <Field
           label="Post Content"
           name="content"
-          component={this.renderField} //--> Pass function reference
+          component={this.renderField} 
         />
       </form>
     );
   }
 }
 
+/**
+ * Validates fields called every re-render cycle
+ * @param {Obj} values Contains the values entered by the user -> {title: 'dsadas', categories: 'blablabla'}
+ */
+function validate(values) {
+  const errors = {};
+
+  // Validates the inputs from 'values' 
+  if (!values.title) {
+    errors.title = 'Enter a title !!';
+  }
+
+  // Is errors is empty, the form is fine to submit
+  // Is errors has *any* props, redux form assumes form is valid
+  return errors;
+
+}
+
 export default reduxForm({ // reduxForm ---> communicate between the component and the formReducer
   form: 'PostsNewForm', // provide a unique name for the form
+  validate,
 })(PostsNew);
